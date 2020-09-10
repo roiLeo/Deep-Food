d62290eede60581bf1aef57faacf4260966f38f1
roiLeo <medina.leo42@gmail.com>
2020-09-10 14:41:14 +0200

    Deep Food init

---
+++ components/FoodImage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="img-detect">
+        <img :src="imageUrl" alt="" width="100%" />
+    </div>
+</template>
+<script>
+export default {
+    name: "FoodImage",
+    props: {
+        imageUrl: {
+            type: String
+        }
+    }
+};
+</script>
+<style></style>
